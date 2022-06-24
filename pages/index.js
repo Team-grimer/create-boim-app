359b4c33ef5ac481a1d3019f7e7888ee8fa4c924
eunoia2040 <eunoia2040@gmail.com>
2022-06-24 12:24:41 +0900

    fix: Update default component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
+import grimer from "../public/team_grimer.png";
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const handleButtonClick = () => {
+    setCount(prev => prev + 1);
+  }
   return (
     <>
       <div>Welcome! boimjs</div>
-      <image src="team_grimer.png" />
+      <img src={grimer} alt="grimer_logo" />
+      <p>{count}</p>
+      <button onClick={handleButtonClick}>Click Me!</button>
     </>
   )
 }
