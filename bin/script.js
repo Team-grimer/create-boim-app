2622dc98e55e9acce156fc624d1957962f8cb9eb
eunoia2040 <eunoia2040@gmail.com>
2022-06-28 02:38:29 +0900

    feat: Update script.js

--- bin/script.js
+++ bin/script.js
@@ -2,41 +2,47 @@
 
 const { spawn } = require("child_process");
 const { program } = require("commander");
+const controller = new AbortController();
+const { signal } = controller;
 const chalk = require("chalk");
-const process = spawn("bash");
+const process = spawn("bash", { signal });
 
 program
   .arguments("<*>")
   .action(async function (action, _) {
-    const command = `
-    cd node_modules/boim_test2 \n
-    npm run ${action} \n
-    `;
+    if (action === "dev" || action === "build" || action === "start") {
+      const command = `
+      cd node_modules/boim_test2 \n
+      npm run ${action} \n
+      `;
 
-    function runScript() {
-      return new Promise(function (resolve, reject) {
-        try {
-          process.stdin.write(command);
-          process.stdin.end();
+      function runScript() {
+        return new Promise(function (resolve, reject) {
+          try {
+            process.stdin.write(command);
+            process.stdin.end();
 
-          process.stdout.on("data", (data) => {
-            const message = data.toString();
-            console.log(message);
-            resolve(message);
-          });
+            process.stdout.on("data", (data) => {
+              const message = data.toString();
+              console.log(message);
+              resolve(message);
+            });
 
-          process.on("close", function (code) {
-            process.kill();
-          });
-        } catch (err) {
-          console.log("error");
-          reject(err);
-        }
-      });
+            process.on("close", function (code) {
+              process.kill();
+            });
+          } catch (err) {
+            console.log("error");
+            reject(err);
+          }
+        });
+      }
+
+      await runScript();
+
+      console.log(chalk.bold.greenBright("Boim-Run-Script"));
+    } else {
+      controller.abort();
     }
-
-    await runScript();
-
-    console.log(chalk.bold.greenBright("Boim-Run-Script"));
   })
   .parse(process.argv);
